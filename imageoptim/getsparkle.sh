c5d6bd21c0c0ecc94bd5c99ded2bab9341d4d9aa
Kornel Lesiński <kornel@geekhood.net>
2013-10-01 19:48:46 +0100

    Fixed Sparkle download script

--- imageoptim/getsparkle.sh
+++ imageoptim/getsparkle.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
-SPAKRLE=$1
+SPARKLE=$1
 
 TARGET_TEMP_DIR=${TARGET_TEMP_DIR:-/tmp/}
 SPARKLETMP=$TARGET_TEMP_DIR/Sparkle.framework
 SPARKLEFALLBACK=${SRCROOT:-/tmp/none/}/Sparkle.framework
 SPARKLEZIP=$TARGET_TEMP_DIR/sparkle.zip
 
-if test -e "$SPAKRLE"; then
+if test -e "$SPARKLE"; then
     exit 0;
 fi
 
-if test -e "$SPAKRLETMP"; then
-    cp -R "$SPAKRLETMP" "$SPAKRLE" && exit 0
+if test ! -d "$(basename "$SPARKLE")"; then
+	mkdir -p "$(basename "$SPARKLE")"
+fi
+
+if test -e "$SPARKLETMP"; then
+    echo COPY TMP cp -R "$SPARKLETMP" "$SPARKLE"
+    cp -R "$SPARKLETMP" "$SPARKLE" && exit 0
 fi
 
 if test -e "$SPARKLEFALLBACK"; then
-    cp -R "$SPARKLEFALLBACK" "$SPAKRLE" && exit 0
+    echo COPY FALLBACK cp -R "$SPARKLEFALLBACK" "$SPARKLE"
+    cp -R "$SPARKLEFALLBACK" "$SPARKLE" && exit 0
 fi
 
 echo Downloading Sparkle
 
-test ! -e "$SPAKRLEZIP" || rm -rf "$SPAKRLEZIP"
-curl http://sparkle.andymatuschak.org/files/Sparkle%201.5b6.zip -o "$SPAKRLEZIP"
-unzip -o "$SPAKRLEZIP" 'With Garbage Collection/*' -d "$TARGET_TEMP_DIR"
-mv "$TARGET_TEMP_DIR/With Garbage Collection/Sparkle.framework" "$SPAKRLETMP"
+test ! -e "$SPARKLEZIP" || rm -rf "$SPARKLEZIP"
+echo curl http://sparkle.andymatuschak.org/files/Sparkle%201.5b6.zip -o "$SPARKLEZIP"
+curl http://sparkle.andymatuschak.org/files/Sparkle%201.5b6.zip -o "$SPARKLEZIP"
+unzip -o "$SPARKLEZIP" 'With Garbage Collection/*' -d "$TARGET_TEMP_DIR"
+echo mv "$TARGET_TEMP_DIR/With Garbage Collection/Sparkle.framework" "$SPARKLETMP"
+mv "$TARGET_TEMP_DIR/With Garbage Collection/Sparkle.framework" "$SPARKLETMP"
 rm -rf "$TARGET_TEMP_DIR/With Garbage Collection"
-rm -rf "$SPAKRLETMP/Versions/A/Resources/fr_CA.lproj"
+rm -rf "$SPARKLETMP/Versions/A/Resources/fr_CA.lproj"
 
-cp -R "$SPAKRLETMP" "$SPAKRLE"
+echo COPY LAST cp -R "$SPARKLETMP" "$SPARKLE"
+cp -R "$SPARKLETMP" "$SPARKLE"
