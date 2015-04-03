7fc73a12ec3f258d65a5578eb8b14b5a10be9b5c
Kornel Lesiński <kornel@geekhood.net>
2015-04-03 22:45:01 +0100

    Download Sparkle 1.9

--- imageoptim/getsparkle.sh
+++ imageoptim/getsparkle.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-SPARKLE=$1
+SPARKLE=${1:-Sparkle.framework}
 
 TARGET_TEMP_DIR=${TARGET_TEMP_DIR:-/tmp/}
 SPARKLETMP=$TARGET_TEMP_DIR/Sparkle.framework
 SPARKLEFALLBACK=${SRCROOT:-/tmp/none/}/Sparkle.framework
-SPARKLEZIP=$TARGET_TEMP_DIR/sparkle.tar.bz2
+SPARKLEZIP=$TARGET_TEMP_DIR/Sparkle-tmp.tar.bz2
 
 if test -e "$SPARKLE"; then
+    echo "$SPARKLE already exists"
     exit 0;
 fi
 
@@ -15,17 +16,21 @@ if test ! -d "$(dirname "$SPARKLE")"; then
 fi
 
 if test -e "$SPARKLETMP"; then
+    echo "Copying Sparkle from $SPARKLETMP"
     cp -R "$SPARKLETMP" "$SPARKLE" && exit 0
 fi
 
 if test -e "$SPARKLEFALLBACK"; then
+    echo "Copying Sparkle from $SPARKLEFALLBACK"
     cp -R "$SPARKLEFALLBACK" "$SPARKLE" && exit 0
 fi
 
-echo Downloading Sparkle
+if test '!' -f "$SPARKLEZIP"; then
+    echo "Downloading Sparkle"
+    curl -L -o "$SPARKLEZIP" https://github.com/sparkle-project/Sparkle/releases/download/1.9.0/Sparkle-1.9.0.tar.bz2 || { rm -rf "$SPARKLEZIP"; exit 1; }
+fi
 
-test ! -e "$SPARKLEZIP" || rm -rf "$SPARKLEZIP"
-curl -L https://github.com/sparkle-project/Sparkle/releases/download/1.9.0/Sparkle-1.9.0.tar.bz2 -o "$SPARKLEZIP" || exit 1
-tar xjvf "$SPARKLEZIP" --strip-components 1 --include '*/Sparkle.framework' -C "$TARGET_TEMP_DIR/" || exit 1
+rm -rf "$TARGET_TEMP_DIR/Sparkle.framework" "$SPARKLE"
+tar xjvf "$SPARKLEZIP" --include './Sparkle.framework' -C "$TARGET_TEMP_DIR/" || exit 1
 
 cp -R "$SPARKLETMP" "$SPARKLE"
