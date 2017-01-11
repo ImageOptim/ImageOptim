145c4fb2f3869f8e208d33649717aedf36877ef8
Kornel Lesiński <kornel@geekhood.net>
2017-01-11 19:00:05 +0000

    Moved files

---
+++ imageoptim/Backend/File.h
@@ -0,0 +1,13 @@
+//
+//  File.h
+//  ImageOptim
+//
+//  Created by Kornel on 11/01/2017.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface File : NSObject
+
+@end
---
+++ imageoptim/Backend/File.m
@@ -0,0 +1,13 @@
+//
+//  File.m
+//  ImageOptim
+//
+//  Created by Kornel on 11/01/2017.
+//
+//
+
+#import "File.h"
+
+@implementation File
+
+@end
