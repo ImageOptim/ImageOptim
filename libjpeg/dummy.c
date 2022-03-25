9315af35451f42d6d0e97d67952f41f9c9782bce
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-25 17:35:38 +0800

    Delete libjpeg directory

--- libjpeg/dummy.c
+++
@@ -1,9 +0,0 @@
-/* stub functions for features that can't be disabled without source modifications */
-void jinit_1pass_quantizer(void) {}
-void jinit_merged_upsampler(void) {}
-void jpeg_idct_1x1(void) {}
-void jpeg_idct_2x2(void) {}
-void jpeg_idct_4x4(void) {}
-void jinit_2pass_quantizer(void) {}
-void jpeg_idct_ifast(void) {}
-void jpeg_fdct_ifast(void) {}
