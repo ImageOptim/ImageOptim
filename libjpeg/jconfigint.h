15dad2f92c5df02c16c5ed996b48e647ea47a892
Kornel <kornel@geekhood.net>
2023-10-14 22:36:13 +0100

    libjpeg update

--- libjpeg/jconfigint.h
+++ libjpeg/jconfigint.h
@@ -1,12 +1,20 @@
 /* jconfigint.h.  Generated from jconfigint.h.in by configure.  */
 /* libjpeg-turbo build number */
-#define BUILD "20150709"
+#define BUILD  "20231014"
+
+/* Compiler's inline keyword */
+#undef inline
 
 /* How to obtain function inlining. */
-#define INLINE inline __attribute__((always_inline))
+#define INLINE  __inline__ __attribute__((always_inline))
+
+/* How to obtain thread-local storage */
+#define THREAD_LOCAL  __thread
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "mozjpeg"
 
 /* Version number of package */
-#define VERSION "3.1"
+#define VERSION  "4.1.5"
+
+#define FALLTHROUGH
